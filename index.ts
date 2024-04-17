b622150b6803636c084b8c78c986b3614353702e
hina2244 <160247675+hina2244@users.noreply.github.com>
2024-04-17 03:56:39 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgBlackBright("***CURRENCY CONVERTOR***"));
+
+const currency: any = {
+  USD: 1, //base currency
+  PKR: 277.54,
+  INR: 83.3,
+  EUR: 0.92,
+  NZD: 1.66,
+  NPR: 133.08,
+  MAD: 10.06,
+  EGP: 47.33,
+  IRR: 42075.0,
+  AFN: 71.23,
+  IQD: 1308.17,
+};
+let userSelection = await inquirer.prompt([
+  {
+    name: "From",
+    message: "SELECT YOUR CURRENCY WHICH YOU WANT TO CONVERT",
+    type: "list",
+    choices: ["USD","PKR", "INR","EUR","NZD","NPR", "MAD","EGP","IRR", "AFN","IQD", ],
+  },
+  {
+    name: "To",
+    message: "SELECT YOUR CURRENCY IN WHICH YOU WANT TO CONVERT",
+    type: "list",
+    choices: ["USD","PKR","INR","EUR","NZD","NPR","MAD","EGP","IRR","AFN","IQD",],
+  },
+  {
+    name: "Amount",
+    message: "HOW MUCH AMOUNT YOU WANT TO CONVERT",
+    type: "number",
+  },
+]);
+
+let fromlist = currency[userSelection.From]; //pkr 277.54
+let tolist = currency[userSelection.To]; //
+let amount = userSelection.Amount; // 5000
+let BaseAmount = amount / fromlist; //5000 /277.54
+let convertedAmount = BaseAmount * tolist;
+let result = Math.round(convertedAmount);
+
+console.log(chalk.bgMagenta(result));
